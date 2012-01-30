fcd4bacc2eb53b5821ae4c62dafa5981600251ae
Lyman Lai <lymanlai2011@gmail.com>
2012-01-30 09:50:32 +0800

    pattern-2

--- jquery.plugin-pattern-1.js
+++ jquery.plugin-pattern-1.js
@@ -1,63 +1,63 @@
-// officer: http://docs.jquery.com/Plugins/Authoring#Data
-(function( $ ){
+// officer: http://docs.jquery.com/Plugins/Authoring
+;(function( $ ){
+    var methods = {
+            init : function( options ) {
+                var defaults = {
+                        'location'         : 'top',
+                        'background-color' : 'blue'
+                    };
+                    
+                return this.each(function(){
+                    var $this = $(this),
+                        data = $this.data('tooltip'),
+                        tooltip = $('<div />', {
+                            text : $this.attr('title')
+                        });
+                 
+                    // If the plugin hasn't been initialized yet
+                    if ( ! data ) {
+                        // Create some defaults, extending them with any options that were provided
+                        var settings = $.extend( {}, defaults, options);
+                        $(this).data('tooltip', {
+                            target : $this,
+                            tooltip : tooltip
+                        });
+                    }
+               });
+            },
+            destroy : function( ) {
+                return this.each(function(){
+                    var $this = $(this),
+                        data = $this.data('tooltip');
 
-  var methods = {
-     init : function( options ) {
+                    // Namespacing FTW
+                    $(window).unbind('.tooltip');
+                    data.tooltip.remove();
+                    $this.removeData('tooltip');
+                })
 
-       return this.each(function(){
-         
-         var $this = $(this),
-             data = $this.data('tooltip'),
-             tooltip = $('<div />', {
-               text : $this.attr('title')
-             });
-         
-         // If the plugin hasn't been initialized yet
-         if ( ! data ) {
-         
-           /*
-             Do more setup stuff here
-           */
+            },
+            reposition : function( ) { 
+                // ... 
+            },
+            show : function( ) { 
+                // ... 
+            },
+            hide : function( ) { 
+                // ... 
+            },
+            update : function( content ) { 
+                // ...
+            }
+        };
 
-           $(this).data('tooltip', {
-               target : $this,
-               tooltip : tooltip
-           });
-
-         }
-       });
-     },
-     destroy : function( ) {
-
-       return this.each(function(){
-
-         var $this = $(this),
-             data = $this.data('tooltip');
-
-         // Namespacing FTW
-         $(window).unbind('.tooltip');
-         data.tooltip.remove();
-         $this.removeData('tooltip');
-
-       })
-
-     },
-     reposition : function( ) { // ... },
-     show : function( ) { // ... },
-     hide : function( ) { // ... },
-     update : function( content ) { // ...}
-  };
-
-  $.fn.tooltip = function( method ) {
-    
-    if ( methods[method] ) {
-      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
-    } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
-    }    
-  
-  };
-
+    $.fn.tooltip = function( method ) {
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+        }    
+    };
 })( jQuery );
